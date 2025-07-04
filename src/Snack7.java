f6f0fb14c74659cac8b59406efc224bb9653c3a9
Michelefort <fortunatomikele@gmail.com>
2025-07-04 16:11:15 +0200

    Snack7

---
+++ src/Snack7.java
@@ -0,0 +1,15 @@
+public class Snack7 {
+    public static void main(String[] args) {
+        
+
+
+        for (int potenza = 2; potenza <= 1000; potenza *= 2) {
+            System.out.println(potenza);
+        }
+
+        
+
+
+    }
+
+}
